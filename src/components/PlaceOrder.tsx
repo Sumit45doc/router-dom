403d1af45c998d286c1b73f5f1a5524f53a5d6c9
Sumit Kumar Sharma <ss8559283@gmail.com>
2023-02-27 01:33:38 +0530

    navigate programmatically

--- src/components/PlaceOrder.tsx
+++ src/components/PlaceOrder.tsx
@@ -1,13 +1,22 @@
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 
 function PlaceOrder() {
     // state from where the page is navigated
     const { state } = useLocation()
+    const navigate = useNavigate()
+    const number = Math.floor(Math.random() * 10)
+    const itemCount = number > 5;
+    
+    const handleOnClick = () => {
+        if (itemCount) navigate('/payment', {replace: true}); 
+    }
 
     return (
         <>
             <Link to={'/payment'} replace>PlaceOrder</Link>
-            <p>Price of order: {state.price}</p>
+            <p>Price of order: {state.price} </p>
+            <p>Item available: {itemCount ? 'available': 'unavailable'}</p>
+            <button onClick={handleOnClick}>Place order programatically</button>
         </>
     )
 }
