b2197fec28c0dd8088ca61ff66808b9f60aa31dc
Sumit Kumar Sharma <ss8559283@gmail.com>
2023-02-27 01:20:41 +0530

    NavLink component to get active class

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import About from './components/About';
 import Home from './components/Home';
+import NavActiveLink from './components/NavActiveLink';
 import Navbar from './components/Navbar';
 import PaymentOrder from './components/PaymentOrder';
 import PlaceOrder from './components/PlaceOrder';
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <NavActiveLink />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='about' element={<About />} />
