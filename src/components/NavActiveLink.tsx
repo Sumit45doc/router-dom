05ad4a50a89b63ffb7b87f0316222f641c98c0c4
Sumit Kumar Sharma <ss8559283@gmail.com>
2023-02-27 01:24:40 +0530

    Navlink active class properties manupilation

--- src/components/NavActiveLink.tsx
+++ src/components/NavActiveLink.tsx
@@ -2,10 +2,18 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 function NavActiveLink() {
+
+    const navStyleLink = ({ isActive }: {isActive: boolean}) => {
+        return {
+            fontWeight: isActive ? 'bold' : 'normal',
+            textDecoration: isActive ? 'none' : 'underline'
+        }
+    }
+
     return (
         <nav>
-            <NavLink to={'/'} >Home</NavLink>
-            <NavLink to={'/about'} > About</NavLink>
+            <NavLink to={'/'} style={navStyleLink}>Home</NavLink>
+            <NavLink to={'/about'} style={navStyleLink}> About</NavLink>
         </nav>
     )
 }
