7b1bf0b212ba0772d443038e75f3ca450b6ca2c5
Sumit Kumar Sharma <ss8559283@gmail.com>
2023-03-10 01:23:37 +0530

    route using useRoutes hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useRoutes, Navigate } from 'react-router-dom';
 import Admin from './components/Admin';
 import FeaturedProducts from './components/FeaturedProducts';
 import Home from './components/Home';
@@ -18,32 +18,92 @@ const LazyAbout = React.lazy(() => import('./components/About'))
 
 function App() {
 
+  // return (
+  //   <>
+  //     <Navbar />
+  //     {/* <NavActiveLink /> */}
+  //     <Routes>
+  //       <Route path="/" element={<Home />} />
+  //       <Route path='about' element={
+  //         <React.Suspense fallback={<>Loading..</>}>
+  //         <LazyAbout />
+  //       </React.Suspense>
+  //       } />
+  //       <Route path='place-order' element={<PlaceOrder />} />
+  //       <Route path="payment" element={<PaymentOrder />} />
+  //       <Route path="products" element={<Products />}>
+  //         <Route index element={<FeaturedProducts />} />
+  //         <Route path="feature" element={<FeaturedProducts />} />
+  //         <Route path="new" element={<NewProducts />} />
+  //       </Route>
+  //       <Route path="users" element={<User />} >
+  //         <Route path=":id" element={<UserDetails  />} />
+  //         <Route path="admin" element={<Admin />} />
+  //       </Route>
+  //       <Route path="*" element={<NotFound />} />
+  //     </Routes>
+  //   </>
+  // );
+
+
+  const element = useRoutes([
+    {
+      path: '/',
+      element: <Home />
+    },
+    {
+      path: 'about',
+      element: (
+        <React.Suspense fallback={<>Loading..</>}>
+          <LazyAbout />
+        </React.Suspense>
+      )
+    },
+    {
+      path: 'products',
+      element: <Products />,
+      children: [
+        {
+          element: <FeaturedProducts />,
+          index: true
+        },
+        {
+          path: 'feature',
+          element: <FeaturedProducts />
+        },
+        {
+          path: 'new',
+          element: <NewProducts />
+        }
+      ]
+    },
+    {
+      path: 'users',
+      element: <User />,
+      children: [
+        {
+          path: ':id',
+          element: <UserDetails />
+        },
+        {
+          path: 'admin',
+          element: <Admin />
+        }
+      ]
+    },
+    {
+      path: '*',
+      element: <NotFound />
+    }
+  ])
+
   return (
     <>
       <Navbar />
-      {/* <NavActiveLink /> */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path='about' element={
-          <React.Suspense fallback={<>Loading..</>}>
-          <LazyAbout />
-        </React.Suspense>
-        } />
-        <Route path='place-order' element={<PlaceOrder />} />
-        <Route path="payment" element={<PaymentOrder />} />
-        <Route path="products" element={<Products />}>
-          <Route index element={<FeaturedProducts />} />
-          <Route path="feature" element={<FeaturedProducts />} />
-          <Route path="new" element={<NewProducts />} />
-        </Route>
-        <Route path="users" element={<User />} >
-          <Route path=":id" element={<UserDetails  />} />
-          <Route path="admin" element={<Admin />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      {element}
     </>
-  );
+  )
+
 }
 
 export default App;
