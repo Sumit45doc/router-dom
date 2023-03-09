213e17b989362930f25537a2d62d48acd588eb6f
Sumit Kumar Sharma <ss8559283@gmail.com>
2023-03-10 00:30:00 +0530

    Dynamic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import About from './components/About';
+import Admin from './components/Admin';
 import FeaturedProducts from './components/FeaturedProducts';
 import Home from './components/Home';
 import NavActiveLink from './components/NavActiveLink';
@@ -9,6 +10,8 @@ import NotFound from './components/NotFound';
 import PaymentOrder from './components/PaymentOrder';
 import PlaceOrder from './components/PlaceOrder';
 import Products from './components/Products';
+import User from './components/User';
+import UserDetails from './components/UserDetails';
 
 function App() {
   return (
@@ -20,12 +23,16 @@ function App() {
         <Route path='about' element={<About />} />
         <Route path='place-order' element={<PlaceOrder />} />
         <Route path="payment" element={<PaymentOrder />} />
-        <Route path="*" element={<NotFound />} />
         <Route path="products" element={<Products />}>
           <Route index element={<FeaturedProducts />} />
           <Route path="feature" element={<FeaturedProducts />} />
           <Route path="new" element={<NewProducts />} />
         </Route>
+        <Route path="users" element={<User />} >
+          <Route path=":id" element={<UserDetails  />} />
+          <Route path="admin" element={<Admin />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
